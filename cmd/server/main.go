6f557bef698483e1d0fb0baf9c979bba7083a242
Sam Tholiya <tholiya.sam@outlook.in>
2021-01-12 11:28:56 +0530

    Expose API for the service at port :80

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"os"
 
+	"github.com/samtholiya/analyserService/internal/controller/api/v1/analyser"
+	"github.com/samtholiya/analyserService/internal/service/analyser/html"
+)
+
+func main() {
+	// Generally I would divide the controllers
+	// Use interface etc
+	// but as there is just one api keep it simple silly
+	mux := http.NewServeMux()
+	crawl := &html.Crawler{}
+	analyser := analyser.Analyser{
+		Crawler: crawl,
+	}
+	mux.HandleFunc("/api/v1/analyser/html", analyser.Post)
+	err := http.ListenAndServe(":80", mux)
+	if err != nil {
+		os.Exit(1)
+	}
 }
